3e60a58bb4ece4bfe208a6474049f8030800143d
webdeveloper2794 <webdeveloper2794@gmail.com>
2024-06-30 18:42:20 +0300

    search input text color

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <h1 className="text-2xl sm:text-4xl text-green-700 py-10 font-serif text-center">Rosululloh <span className="text-gray-900 font-bold">صلى الله عليه و سلم</span>  Hadislari top&apos;lami</h1>
       <label className="input input-bordered flex items-center gap-2 bg-white input-accent w-full lg:w-1/2">
-        <input type="text" className="grow input-bordered input-accent input-sm " placeholder="Search" />
+        <input type="text" className="grow input-bordered input-accent input-sm text-green-900" placeholder="Search" />
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 16 16"
