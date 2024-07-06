0638c9d811f4fa3617c1a99b386fa73006fdfa43
Abdurrahman <abdurrahman@newbilisim.com>
2024-07-06 04:00:36 +0300

    update

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import Link from 'next/link'
+import AdminNavbar from '@/components/adminNavbar/adminNavbar'
+
 const Adminlayout = ({children}) => {
   return (
-    <div className="flex min-h-screen pt-16">
-      <aside className="w-64 bg-transparent text-green-500 flex flex-col shadow-lg">
-        <div className="p-4 text-lg font-semibold pt-16">Admin Dashboard</div>
-        <nav className="flex flex-col p-4 space-y-4">
-          <Link href="/admin" className="hover:bg-gray-700 p-2 rounded">
+    <div className="flex min-h-screen pt-16 ">
+      
+      <main className="flex-1 p-8 bg-gray-100/50 relative scroll-auto">
+      <aside className="w-40 bg-transparent text-gray-700 flex flex-col shadow-lg fixed z-50">
+        <nav className="flex flex-col p-2 space-y-4  ">
+          <Link href="/admin" className="hover:bg-green-500 hover:text-white p-2 rounded text-sm w-full">
            Home
           </Link>
-          <Link href="/admin/addbook" className="hover:bg-gray-700 p-2 rounded">
+          <Link href="/admin/addbook" className="hover:bg-green-500 hover:text-white p-2 rounded text-sm">
            Add New Book
           </Link>
-          <Link href="/admin/book" className="hover:bg-gray-700 p-2 rounded">
+          <Link href="/admin/book" className="hover:bg-green-500 hover:text-white p-2 rounded text-sm">
             Book Lists
           </Link>
         </nav>
       </aside>
-      <main className="flex-1 p-8 bg-gray-100/50 relative">{children}</main>
+      <AdminNavbar/>
+      {children}
+      </main>
     </div>
   )
 }
