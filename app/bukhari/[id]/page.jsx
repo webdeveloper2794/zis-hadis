8d0c395578dcf62013343759b3e05d48bb138df9
Abdurrahman <abdurrahman@newbilisim.com>
2024-07-11 23:59:59 +0300

    update

--- app/bukhari/[id]/page.jsx
+++ app/bukhari/[id]/page.jsx
@@ -1,71 +1,119 @@
 import React from 'react'
 
+const data = {
+  chapterId: "6686ef3ecbb6b811d9c85305",
+  chapterNumber:1,
+  chapterTitle: {
+    uz: "Risolat",
+    ar: "كتاب بدء الوحى"
+  },
+  hadiths: [
+    {
+      _id: "hadithObjectId1",
+      subChapterNumber: 1,
+      subChapterTitle: {
+        uz: "Ilmning boshlanishi",
+        ar: "باب كَيْفَ كَانَ بَدْءُ الْوَحْىِ إِلَى رَسُولِ اللَّهِ صلى الله عليه وسلم"
+      },
+      arabic_ayah: "إِنَّا أَوْحَيْنَا إِلَيْكَ كَمَا أَوْحَيْنَا إِلَى نُوحٍ وَالنَّبِيِّينَ مِنْ بَعْدِهِ",
+      narratorName: "Umar bin Al-Khattab",
+      hadith: {
+        arabic: "حَدَّثَنَا الْحُمَيْدِيُّ عَبْدُ اللَّهِ بْنُ الزُّبَيْرِ ، قَالَ : حَدَّثَنَا سُفْيَانُ ، قَالَ : حَدَّثَنَا يَحْيَى بْنُ سَعِيدٍ الْأَنْصَارِيُّ ، قَالَ : أَخْبَرَنِي مُحَمَّدُ بْنُ إِبْرَاهِيمَ التَّيْمِيُّ ، أَنَّهُ سَمِعَ عَلْقَمَةَ بْنَ وَقَّاصٍ اللَّيْثِيَّ ، يَقُولُ : سَمِعْتُ عُمَرَ بْنَ الْخَطَّابِ رَضِيَ اللَّهُ عَنْهُ عَلَى الْمِنْبَرِ، قَالَ : سَمِعْتُ رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ، يَقُولُ : ' إِنَّمَا الْأَعْمَالُ بِالنِّيَّاتِ، وَإِنَّمَا لِكُلِّ امْرِئٍ مَا نَوَى، فَمَنْ كَانَتْ هِجْرَتُهُ إِلَى دُنْيَا يُصِيبُهَا أَوْ إِلَى امْرَأَةٍ يَنْكِحُهَا، فَهِجْرَتُهُ إِلَى مَا هَاجَرَ إِلَيْهِ '",
+        uzbek: "Men Rasululloh (ﷺ)ning shunday deganini eshitdim: 'Amallarning mukofoti niyatlarga bog'liq va har bir kishi niyatiga qarab mukofot oladi. Kim dunyoviy manfaatlar uchun yoki bir ayolga uylanish uchun hijrat qilgan bo'lsa, uning hijrati nima uchun hijrat qilgan bo'lsa, shunga qarab bo'ladi.'",
+        english: "I heard Allah's Messenger (ﷺ) saying, 'The reward of deeds depends upon the intentions and every person will get the reward according to what he has intended. So whoever emigrated for worldly benefits or for a woman to marry, his emigration was for what he emigrated for.'",
+        russian: "Мен Расулуллоҳ (ﷺ)нинг шундай деганини эшитдим: 'Амалларнинг мукофоти ниятларга боғлиқ ва ҳар бир киши ниятига қараб мукофот олади. Ким дунёвий манфаатлар учун ёки бир аёлга уйланиш учун ҳижрат қилган бўлса, унинг ҳижрати нима учун ҳижрат қилган бўлса, шунга қараб бўлади.'"
+      },
+      hadisNumber: 1
+    },
+    {
+      _id: "hadithObjectId2",
+      subChapterNumber: 2,
+      subChapterTitle: {
+        uz: "",
+        ar: "باب "
+      },
+      arabic_ayah: "",
+      narratorName: "Aisha",
+      hadith: {
+        arabic: "حَدَّثَنَا عَبْدُ اللَّهِ بْنُ يُوسُفَ، قَالَ أَخْبَرَنَا مَالِكٌ، عَنْ هِشَامِ بْنِ عُرْوَةَ، عَنْ أَبِيهِ، عَنْ عَائِشَةَ أُمِّ الْمُؤْمِنِينَ ـ رضى الله عنها ـ أَنَّ الْحَارِثَ بْنَ هِشَامٍ ـ رضى الله عنه ـ سَأَلَ رَسُولَ اللَّهِ صلى الله عليه وسلم فَقَالَ يَا رَسُولَ اللَّهِ كَيْفَ يَأْتِيكَ الْوَحْىُ فَقَالَ رَسُولُ اللَّهِ صلى الله عليه وسلم ‏ \"أَحْيَانًا يَأْتِينِي مِثْلَ صَلْصَلَةِ الْجَرَسِ ـ وَهُوَ أَشَدُّهُ عَلَىَّ ـ فَيُفْصَمُ عَنِّي وَقَدْ وَعَيْتُ عَنْهُ مَا قَالَ، وَأَحْيَانًا يَتَمَثَّلُ لِيَ الْمَلَكُ رَجُلاً فَيُكَلِّمُنِي فَأَعِي مَا يَقُولُ \"‏‏.‏ قَالَتْ عَائِشَةُ رضى الله عنها وَلَقَدْ رَأَيْتُهُ يَنْزِلُ عَلَيْهِ الْوَحْىُ فِي الْيَوْمِ الشَّدِيدِ الْبَرْدِ، فَيَفْصِمُ عَنْهُ وَإِنَّ جَبِينَهُ لَيَتَفَصَّدُ عَرَقًا‏\"‏‏.‏",
+        uzbek: "حَدَّثَنَا عَبْدُ اللَّهِ بْنُ يُوسُفَ، قَالَ أَخْبَرَنَا مَالِكٌ، عَنْ هِشَامِ بْنِ عُرْوَةَ، عَنْ أَبِيهِ، عَنْ عَائِشَةَ أُمِّ الْمُؤْمِنِينَ ـ رضى الله عنها ـ أَنَّ الْحَارِثَ بْنَ هِشَامٍ ـ رضى اللَّهُ عَنْهُ ـ سَأَلَ رَسُولَ اللَّهِ صلى الله عليه وسلم فَقَالَ يَا رَسُولَ اللَّهِ كَيْفَ يَأْتِيكَ الْوَحْىُ فَقَالَ رَسُولُ اللَّهِ صلى الله عليه وسلم ‏ \"أَحْيَانًا يَأْتِينِي مِثْلَ صَلْصَلَةِ الْجَرَسِ ـ وَهُوَ أَشَدُّهُ عَلَىَّ ـ فَيُفْصَمُ عَنِّي وَقَدْ وَعَيْتُ عَنْهُ مَا قَالَ، وَأَحْيَانًا يَتَمَثَّلُ لِيَ الْمَلَكُ رَجُلاً فَيُكَلِّمُنِي فَأَعِي مَا يَقُولُ \"‏‏.‏ قَالَتْ عَائِشَةُ رضى اللَّهُ عَنْهَا وَلَقَدْ رَأَيْتُهُ يَنْزِلُ عَلَيْهِ الْوَحْىُ فِي الْيَوْمِ الشَّدِيدِ الْبَرْدِ، فَيَفْصِمُ عَنْهُ وَإِنَّ جَبِينَهُ لَيَتَفَصَّدُ عَرَقًا",
+        english: "Narrated 'Aisha: (the mother of the faithful believers) Al-Harith bin Hisham asked Allah's Messenger (ﷺ) \"O Allah's Messenger (ﷺ)! How is the Divine Inspiration revealed to you?\" Allah's Messenger (ﷺ) replied, \"Sometimes it is (revealed) like the ringing of a bell, this form of Inspiration is the hardest of all and then this state passes off after I have grasped what is inspired. Sometimes the Angel comes in the form of a man and talks to me and I grasp whatever he says.\" 'Aisha added: Verily I saw the Prophet (ﷺ) being inspired divinely on a very cold day and noticed the sweat dropping from his forehead (as the Inspiration was over).\"",
+        russian: "روي عن عائشة (أم المؤمنين) أن الحارث بن هشام سأل رسول الله (صلى الله عليه وسلم) يا رسول الله! كيف يأتيك الوحي؟ فقال رسول الله (صلى الله عليه وسلم) \"أحيانًا يأتيني مثل صلصلة الجرس - وهو أشد علي - فيفصم عني وقد وعيت عنه ما قال، وأحيانًا يتمثل لي الملك رجلا فيكلمني فأعي ما يقول\". وقالت عائشة: إني رأيته ينزل عليه الوحي في يوم شديد البرد، فيفصم عنه، وإن جبينه ليتفصد عرقًا."
+      },
+      hadisNumber: 3
+    }
+  ]
+};
 const Page = () => {
+  const { chapterNumber, chapterTitle, hadiths } = data;
+  
+  
   return (
-    <div className='text-4xl text-slate-900 text-center min-h-screen pt-20 w-full p-4 md:p-10 md:pt-20 max-w-screen-xl'>
-      <header className='w-full   flex justify-between'>
-        <h1 className='text-xl'><span className='m-1'>1.</span>Risolat</h1>
-        <h1 className='text-xl'>كتاب بدء الوحى</h1>
-      </header>
-      <section className='flex flex-wrap justify-between my-4 '>
-        <h1 className='text-base'><span className='m-2'>1.Bob: </span>How the Divine Revelation started being revealed to Allah&apos;s Messenger</h1>
-        <h1 className='text-lg text-right '> باب كَيْفَ كَانَ بَدْءُ الْوَحْىِ إِلَى رَسُولِ اللَّهِ صلى الله عليه وسلم </h1>
-      </section>
-      <section className='flex flex-wrap justify-between my-4'>
-        <h1 className='text-xl text-right  w-full' dir="rtl"> وَقَوْلُ اللَّهِ جَلَّ ذِكْرُهُ: إِنَّا أَوْحَيْنَا إِلَيْكَ كَمَا أَوْحَيْنَا إِلَى نُوحٍ وَالنَّبِيِّينَ مِنْ بَعْدِهِ</h1>
-      </section>
-      <main className='bg-white shadow-xl p-2 text-left w-full rounded-md'>
-        <p className='text-sm font-semibold text-green-900'>Umar ibn Al-Xattobdan:</p>
+    <div className='text-4xl text-slate-900 text-center min-h-screen pt-20 w-full p-4 md:p-10 md:pt-20 max-w-screen-lg'>
+    
+        <div>
+          <header className='w-full flex justify-between'>
+            <h1 className='text-xl'>
+              <span className='m-1'>{chapterNumber}.</span>{chapterTitle.uz}
+            </h1>
+            <h1 className='text-xl'>{chapterTitle.ar}</h1>
+          </header>
+          {hadiths.map((hadith, index) => (
+        <div key={hadith._id}>
+          <section className='flex flex-wrap justify-between my-4 '>
+            <h1 className='text-base'><span className='m-2'>{hadith.subChapterNumber}.Bob: </span>{hadith.subChapterTitle.uz}</h1>
+            <h1 className='text-lg text-right '> {hadith.subChapterTitle.ar} </h1>
+          </section>
+          <section className='flex flex-wrap justify-between my-4'>
+            <h1 className='text-xl text-right w-full' dir="rtl"> {hadith.arabic_ayah}</h1>
+          </section>
+          <main className='bg-white shadow-xl p-2 text-left w-full rounded-md'>
+            <p className='text-sm font-semibold text-green-900 p-2'>{hadith.narratorName}dan:</p>
 
-        <article className='flex flex-col md:flex-row justify-between items-center gap-2'>
-          <div role="tablist" className="tabs tabs-boxed bg-white max-w-[40rem]">
-            <input type="radio" name="my_tabs_1" role="tab" className="tab text-gray-800" aria-label="O'zbekcha" defaultChecked/>
-            <div role="tabpanel" className="tab-content p-2   ">
-              <p className='text-sm md:text-sm text-justify'>
-                Men Rasululloh (ﷺ)ning shunday deganini eshitdim: &apos;Amallarning mukofoti niyatlarga bog&apos;liq va har bir kishi niyatiga qarab mukofot oladi. Kim dunyoviy manfaatlar uchun yoki bir ayolga uylanish uchun hijrat qilgan bo&apos;lsa, uning hijrati nima uchun hijrat qilgan bo&apos;lsa, shunga qarab bo&apos;ladi.&apos;
+            <article className='flex flex-col md:flex-row justify-between items-center gap-2'>
+              <div role="tablist" className="tabs tabs-boxed bg-white max-w-[40rem]">
+                <input type="radio" name={`my_tabs_${index}`} role="tab" className="tab text-gray-800" aria-label="O'zbekcha" defaultChecked/>
+                <div role="tabpanel" className="tab-content p-2">
+                  <p className='text-sm md:text-sm text-justify'>
+                    {hadith.hadith.uzbek}
+                  </p>
+                </div>
+
+                <input type="radio" name={`my_tabs_${index}`} role="tab" className="tab" aria-label="Узбекча" />
+                <div role="tabpanel" className="tab-content p-2">
+                  <p className='text-sm text-justify'>
+                    {hadith.hadith.russian}
+                  </p>
+                </div>
+
+                <input type="radio" name={`my_tabs_${index}`} role="tab" className="tab" aria-label="English" />
+                <div role="tabpanel" className="tab-content p-2">
+                  <p className='text-sm text-justify'>
+                    {hadith.hadith.english}
+                  </p>
+                </div>
+              </div>
+              <div className='p-2 max-w-[40rem] pt-10'>
+                <p className='text-base text-justify' style={{ textAlignLast: "right"}}>
+                  {hadith.hadith.arabic}
+                </p>
+              </div>
+            </article>
+            <footer className='p-4'>
+              <p className='text-xs text-left text-green-900'>
+                Sahih al-Buxoriy 1-jild
               </p>
-            </div>
-
-            <input
-              type="radio"
-              name="my_tabs_1"
-              role="tab"
-              className="tab"
-              aria-label="Узбекча"
-               />
-            <div role="tabpanel" className="tab-content p-2">
-              <p className='text-sm  text-justify'>
-                Мен Расулуллоҳ (ﷺ)нинг шундай деганини эшитдим: &apos;Амалларнинг мукофоти ниятларга боғлиқ ва ҳар бир киши ниятига қараб мукофот олади. Ким дунёвий манфаатлар учун ёки бир аёлга уйланиш учун ҳижрат қилган бўлса, унинг ҳижрати нима учун ҳижрат қилган бўлса, шунга қараб бўлади.&apos;
+              <p className='text-xs text-left text-green-900'>
+                1-kitob, {hadith.hadisNumber}-hadis
               </p>
-            </div>
-
-            <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="English" />
-            <div role="tabpanel" className="tab-content p-2">
-              <p className='text-sm text-justify'>
-                I heard Allah&apos;s Messenger (ﷺ) saying, &quot;The reward of deeds depends upon the intentions and every person will get the reward according to what he has intended. So whoever emigrated for worldly benefits or for a woman to marry, his emigration was for what he emigrated for.&quot;
-              </p>
-            </div>
-          </div>
-          <div className='p-2 max-w-[40rem] pt-10'>
-            <p className='text-base text-justify' style={{ textAlignLast: "right"}}>
-              حَدَّثَنَا الْحُمَيْدِيُّ عَبْدُ اللَّهِ بْنُ الزُّبَيْرِ ، قَالَ : حَدَّثَنَا سُفْيَانُ ، قَالَ : حَدَّثَنَا يَحْيَى بْنُ سَعِيدٍ الْأَنْصَارِيُّ ، قَالَ : أَخْبَرَنِي مُحَمَّدُ بْنُ إِبْرَاهِيمَ التَّيْمِيُّ ، أَنَّهُ سَمِعَ عَلْقَمَةَ بْنَ وَقَّاصٍ اللَّيْثِيَّ ، يَقُولُ : سَمِعْتُ عُمَرَ بْنَ الْخَطَّابِ رَضِيَ اللَّهُ عَنْهُ عَلَى الْمِنْبَرِ، قَالَ : سَمِعْتُ رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ، يَقُولُ : &quot; إِنَّمَا الْأَعْمَالُ بِالنِّيَّاتِ، وَإِنَّمَا لِكُلِّ امْرِئٍ مَا نَوَى، فَمَنْ كَانَتْ هِجْرَتُهُ إِلَى دُنْيَا يُصِيبُهَا أَوْ إِلَى امْرَأَةٍ يَنْكِحُهَا، فَهِجْرَتُهُ إِلَى مَا هَاجَرَ إِلَيْهِ &quot;
-            </p>
-          </div>
-
-        </article>
-        <footer className='p-4'>
-          <p className='text-xs text-left text-green-900'>
-            Sahih al-Buxoriy 1-jild
-          </p>
-          <p className='text-xs text-left text-green-900'>
-            1-kitob, 1-hadis
-          </p>
-
-
-        </footer>
-      </main>
+            </footer>
+          </main>
+        </div>
+      ))}
+        </div>
+     
     </div>
   )
 }
 
-export default page
+export default Page
