dbdb0952c0cc1ec688cbf3ae8939e45cb42f9605
Abdurrahman <abdurrahman@newbilisim.com>
2024-07-11 21:35:04 +0300

    update

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,20 +1,13 @@
 import React from 'react'
-import Link from 'next/link'
-import AdminNavbar from '@/components/adminNavbar/adminNavbar'
 import AdminLayout from '@/components/AdminLayout/AdminLayout'
 const Adminlayout = ({ children }) => {
   return (
 
     <AdminLayout>
-      
         <main className="flex-1  bg-gray-100/50 relative">
           {children}
         </main>
-
-
-     
     </AdminLayout>
   )
 }
-
 export default Adminlayout
--- app/bukhari/[id]/page.jsx
+++ app/bukhari/[id]/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const page = () => {
+const Page = () => {
   return (
     <div className='text-4xl text-slate-900 text-center min-h-screen pt-20 w-full p-4 md:p-10 md:pt-20 max-w-screen-xl'>
       <header className='w-full   flex justify-between'>
@@ -8,7 +8,7 @@ const page = () => {
         <h1 className='text-xl'>كتاب بدء الوحى</h1>
       </header>
       <section className='flex flex-wrap justify-between my-4 '>
-        <h1 className='text-base'><span className='m-2'>1. Bob: </span> How the Divine Revelation started being revealed to Allah&apos;s Messenger</h1>
+        <h1 className='text-base'><span className='m-2'>1.Bob: </span>How the Divine Revelation started being revealed to Allah&apos;s Messenger</h1>
         <h1 className='text-lg text-right '> باب كَيْفَ كَانَ بَدْءُ الْوَحْىِ إِلَى رَسُولِ اللَّهِ صلى الله عليه وسلم </h1>
       </section>
       <section className='flex flex-wrap justify-between my-4'>
@@ -34,20 +34,20 @@ const page = () => {
               aria-label="Узбекча"
                />
             <div role="tabpanel" className="tab-content p-2">
-              <p className='text-sm text-left'>
+              <p className='text-sm  text-justify'>
                 Мен Расулуллоҳ (ﷺ)нинг шундай деганини эшитдим: &apos;Амалларнинг мукофоти ниятларга боғлиқ ва ҳар бир киши ниятига қараб мукофот олади. Ким дунёвий манфаатлар учун ёки бир аёлга уйланиш учун ҳижрат қилган бўлса, унинг ҳижрати нима учун ҳижрат қилган бўлса, шунга қараб бўлади.&apos;
               </p>
             </div>
 
             <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="English" />
             <div role="tabpanel" className="tab-content p-2">
-              <p className='text-sm text-left'>
+              <p className='text-sm text-justify'>
                 I heard Allah&apos;s Messenger (ﷺ) saying, &quot;The reward of deeds depends upon the intentions and every person will get the reward according to what he has intended. So whoever emigrated for worldly benefits or for a woman to marry, his emigration was for what he emigrated for.&quot;
               </p>
             </div>
           </div>
           <div className='p-2 max-w-[40rem] pt-10'>
-            <p className='text-base text-right'>
+            <p className='text-base text-justify' style={{ textAlignLast: "right"}}>
               حَدَّثَنَا الْحُمَيْدِيُّ عَبْدُ اللَّهِ بْنُ الزُّبَيْرِ ، قَالَ : حَدَّثَنَا سُفْيَانُ ، قَالَ : حَدَّثَنَا يَحْيَى بْنُ سَعِيدٍ الْأَنْصَارِيُّ ، قَالَ : أَخْبَرَنِي مُحَمَّدُ بْنُ إِبْرَاهِيمَ التَّيْمِيُّ ، أَنَّهُ سَمِعَ عَلْقَمَةَ بْنَ وَقَّاصٍ اللَّيْثِيَّ ، يَقُولُ : سَمِعْتُ عُمَرَ بْنَ الْخَطَّابِ رَضِيَ اللَّهُ عَنْهُ عَلَى الْمِنْبَرِ، قَالَ : سَمِعْتُ رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ، يَقُولُ : &quot; إِنَّمَا الْأَعْمَالُ بِالنِّيَّاتِ، وَإِنَّمَا لِكُلِّ امْرِئٍ مَا نَوَى، فَمَنْ كَانَتْ هِجْرَتُهُ إِلَى دُنْيَا يُصِيبُهَا أَوْ إِلَى امْرَأَةٍ يَنْكِحُهَا، فَهِجْرَتُهُ إِلَى مَا هَاجَرَ إِلَيْهِ &quot;
             </p>
           </div>
