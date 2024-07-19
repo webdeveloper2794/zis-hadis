f3b6bba26d4ebaf56653b551b7c59b75daf2be85
webdeveloper2794 <webdeveloper2794@gmail.com>
2024-07-19 20:23:17 +0300

    update chapter

--- app/admin/book/page.jsx
+++ app/admin/book/page.jsx
@@ -146,7 +146,7 @@ export default function page() {
   ];
   return (
     <>
-      <h1 className="text-2xl font-bold mb-6">Books</h1>
+      <h1 className="text-2xl font-bold mb-6">Booksss</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 text-green-900 ">
         {books.map((book) => (
           <div key={book._id} className="bg-white p-4 rounded shadow">
--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -10,30 +10,34 @@ export default function Page() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const router = useRouter();
- 
+
 
   useEffect(() => {
     const fetchBooks = async () => {
-        try {
-            const response = await axios.get("/api/books");
-            setBooks(response.data.books);
-            setLoading(false);
-        } catch (error) {
-            setError(error);
-            setLoading(false);
-        }
+      try {
+        const response = await axios.get("/api/books");
+        setBooks(response.data.books);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
     };
 
     fetchBooks();
-}, []);
+  }, []);
   const handleEdit = (bookId) => {
     // Navigate to the edit book page
     router.push(`/admin/book/${bookId}`);
     // router.push(`/admin/editbook`);
   };
+
+
   const handleRemove = async (bookId) => {
     try {
-      const response = await axios.delete(`/api/books?id=${bookId}`);
+      const response = await axios.delete(`/api/books`, {
+        data: { id: bookId }, // Include the book ID in the request body
+      });
       if (response.status === 200) {
         setBooks(books.filter(book => book._id !== bookId));
         alert("Kitob muvaffaqiyatli o'chirildi!");
@@ -48,6 +52,7 @@ export default function Page() {
     <>
       <div>
         <h1 className="text-2xl font-bold mb-6 text-green-900">Books</h1>
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 text-gray-700">
           {books && books.map((book) => (
             <div key={book._id} className="bg-white p-4 rounded shadow flex flex-col justify-between">
@@ -66,9 +71,26 @@ export default function Page() {
                       <FaEdit className="text-lg" />
                     </button>
                   </div>
+                  <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                    <div className="modal-box">
+                      <h3 className="font-bold text-lg">Diqqat!</h3>
+                      <p className="py-4">Bu kitobni o'chirishga aminmisiz?</p>
+                      <div className="modal-action">
+                        <form method="dialog">
+                          <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 hover:bg-red-700 hover:text-white">✕</button>
+                          </form>
+                          {/* if there is a button in form, it will close the modal */}
+                          <button className="btn text-red-500 hover:bg-red-700 hover:text-white" onClick={() => handleRemove(book._id)}>Ok</button>
+                        </form>
+                      </div>
+                    </div>
+                  </dialog>
                   <div className="lg:tooltip" data-tip="Remove">
                     <button
-                      onClick={() => handleRemove(book._id)}
+                      // onClick={() => handleRemove(book._id)}
+                      onClick={() => document.getElementById('my_modal_5').showModal()}
                       className=" text-red-500 hover:text-red-700"
                     >
                       <CiSquareRemove className="text-xl" />
@@ -80,7 +102,7 @@ export default function Page() {
               </div>
               <p className="text-sm">{book.biography}</p>
               <div className="mt-4">
-                <Link href={`/admin/book/${book._id}`} className="text-green-500 hover:underline">
+                <Link href={`/admin/book/${book._id}/chapter`} className="text-green-500 hover:underline">
                   Manage Chapters
                 </Link>
               </div>
