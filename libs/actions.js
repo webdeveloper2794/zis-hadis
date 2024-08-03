c87b5e670b69707442876881dfd54c80ff0eaf9d
webdeveloper2794 <webdeveloper2794@gmail.com>
2024-08-04 00:02:00 +0300

    another update

--- libs/actions.js
+++ libs/actions.js
@@ -1,19 +1,19 @@
-'use server'
+// 'use server'
  
-import { signIn } from '@/auth'
+// import { signIn } from '@/auth'
  
-export async function authenticate(_currentState, formData) {
-  try {
-    await signIn('credentials', formData)
-  } catch (error) {
-    if (error) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.'
-        default:
-          return 'Something went wrong.'
-      }
-    }
-    throw error
-  }
-}
+// export async function authenticate(_currentState, formData) {
+//   try {
+//     await signIn('credentials', formData)
+//   } catch (error) {
+//     if (error) {
+//       switch (error.type) {
+//         case 'CredentialsSignin':
+//           return 'Invalid credentials.'
+//         default:
+//           return 'Something went wrong.'
+//       }
+//     }
+//     throw error
+//   }
+// }
